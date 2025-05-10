d96509ee31d3a44f64613db0706da2d21bd89964
Chel <cdeszz8@gmail.com>
2025-05-10 15:52:13 -0600

    Actualizar el mapeo de servos y la lógica de control para reflejar la reducción de servos de 30 a 22 en MEGA2560.ino, arduino_control.html, index.html y tables.html.

--- apps/arduino/Codigo_arduino/MEGA2560/MEGA2560.ino
+++ apps/arduino/Codigo_arduino/MEGA2560/MEGA2560.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 
-//192.168.0.21
+//192.168.0.21  en IZZI CASA
+//192.168.161.85 en HOTSPOT
 
 Servo servos[20];  // Array para 20 servos (excluimos pines 14 y 15)
 int servo_pins[20] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21};
